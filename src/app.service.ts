7c1eb2e0c221e89a72f49b92aee13c23a67599a0
6amson <bunmigrey@icloud.com>
2025-01-30 05:12:18 +0100

    k8

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Injectable, Post } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  @Get('mono')
+  getHealth(): string {
+    return 'Hello mono!';
+  }
 }
+
+
+  
